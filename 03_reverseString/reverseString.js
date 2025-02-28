e5a4016aed6a624f59bcd824236d9de56a045856
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-02-28 20:14:19 +0500

    Refactor reverseString function to improve implementation and add console log for testing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const splitedString = string.split("");
+  const reversedString = splitedString.reverse();
+  const rejoinedString = reversedString.join("");
+  return rejoinedString;
 };
+
+console.log(reverseString("Oye"));
 
 // Do not edit below this line
 module.exports = reverseString;
