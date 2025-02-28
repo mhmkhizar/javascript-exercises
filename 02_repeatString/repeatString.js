9c1fb2553237fc2451731eafaa37470bcf845315
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-02-28 19:59:45 +0500

    Implement repeatString function to handle negative numbers and improve string repetition logic

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let repeatedString = "";
 
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    repeatedString += string;
+  }
+
+  return repeatedString;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
