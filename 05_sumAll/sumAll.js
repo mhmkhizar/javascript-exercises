79199ed89f681c66e957be383f2ce1382b4c70d3
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-03-02 18:16:55 +0500

    Refactor sumAll function to improve argument validation and enhance summation logic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (arg1, arg2) {
+  if (
+    arg1 < 0 ||
+    arg2 < 0 ||
+    !Number.isInteger(arg1) ||
+    !Number.isInteger(arg2)
+  )
+    return "ERROR";
 
+  let firstNumber = arg1;
+  let secondNumber = arg2;
+  let sum = 0;
+
+  if (arg1 > arg2) {
+    firstNumber = arg2;
+    secondNumber = arg1;
+  }
+
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
