984430836736ff7afcfea932f43eb900cd1268b0
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-03-23 00:11:36 +0500

    Refactor palindromes function to accept a string parameter and improve logic for palindrome checking

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleaned = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const cleanedAndReversed = cleaned.split("").reverse().join("");
+  return cleaned === cleanedAndReversed;
 };
 
 // Do not edit below this line
