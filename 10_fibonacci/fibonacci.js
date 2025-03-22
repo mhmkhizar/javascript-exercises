3af33e082d706bd6fc74c8a8a6de3580ccb53613
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-03-23 00:58:13 +0500

    Refactor fibonacci function to accept an index parameter and handle negative inputs

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  if (index < 0) return "OOPS";
 
+  const sequence = [0, 1];
+  for (let i = 2; i <= parseFloat(index); i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+
+  return sequence[index];
 };
 
 // Do not edit below this line
