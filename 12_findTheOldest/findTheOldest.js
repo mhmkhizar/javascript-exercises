bfe49d7075e83e8e51722cb8ebc541fd54bcadc3
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-03-23 06:05:28 +0500

    Refactor findTheOldest function to accept an array parameter and improve age calculation logic

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  return array
+    .map((person) => ({
+      ...person,
+      yearOfDeath: person.yearOfDeath || new Date().getFullYear(),
+    }))
+    .sort((a, b) => {
+      const aAge = a.yearOfDeath - a.yearOfBirth;
+      const bAge = b.yearOfDeath - b.yearOfBirth;
+      return bAge - aAge;
+    })[0];
 };
 
 // Do not edit below this line
